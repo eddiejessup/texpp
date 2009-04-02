b90af0fdb9d1f9b831b3f08ac7258d7332d9bbe1
ks.vladimir <ks.vladimir@7866b4b4-0b4f-11de-a021-8b77bb4f2096>
2009-04-02 18:51:21 +0000

    Merged changes from trunk.

--- branches/macro_expansion/tests/tex/test_source.cc
+++ branches/macro_expansion/tests/tex/test_source.cc
@@ -84,7 +84,7 @@ void test_source(const char* testfile)
     if(n != jobname.npos)
         jobname = jobname.substr(0, n);
 
-    std::istream* file = new std::ifstream(testfile, std::fstream::in);
+    shared_ptr<std::istream> file(new std::ifstream(testfile, std::fstream::in));
     BOOST_REQUIRE_MESSAGE( !file->fail(), "Can not open input file" );
 
     file->seekg(0, std::ios::end);
@@ -121,7 +121,7 @@ void test_source(const char* testfile)
     std::ofstream outFile((jobname+".src.pp").c_str());
     outFile.write(source.c_str(), source.size());
 
-    delete buf;
+    delete[] buf;
     /*
     string cmd1 = string(tex_executable)
                     + " -ini -interaction nonstopmode"
