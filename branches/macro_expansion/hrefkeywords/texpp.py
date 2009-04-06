caf4f6085c126fa04f52576688ecd1b316ef3596
ks.vladimir <ks.vladimir@7866b4b4-0b4f-11de-a021-8b77bb4f2096>
2009-04-06 00:06:58 +0000

    Merged changes from trunk.

--- branches/macro_expansion/hrefkeywords/texpp.py
+++ branches/macro_expansion/hrefkeywords/texpp.py
@@ -3,6 +3,17 @@
 import sys
 import texpy
 
+def get_text(node, whitelist):
+    text = ''
+
+    for name, child in node.children():
+        if child.type() in ('text_word', 'text_character', 'text_space'):
+            text += child.value()
+        elif not whitelist or child.type() in whitelist:
+            text += get_text(child, whitelist)
+
+    return text
+
 def main():
     """ Main routine """
     # Define command line options
@@ -10,8 +21,12 @@ def main():
     optparser = OptionParser(usage='%prog [options] texfile')
     optparser.add_option('-t', '--print-tree', action='store_true',
                                     help='print document tree before exit')
+    optparser.add_option('-x', '--print-text', action='store_true',
+                                    help='print document text before exit')
     optparser.add_option('-s', '--use-stubs', action='store_true',
                                     help='load stubs from latexstubs.py')
+    optparser.add_option('-q', '--queit', action='store_true',
+                                    help='hide parser log messaged')
 
     # Parse command line options
     opt, args = optparser.parse_args()
@@ -33,20 +48,33 @@ def main():
     else:
         optparser.error('Wrong command line arguments')
         
-    # Create the parser
-    parser = texpy.Parser(filename, fileobj, interactive,
-                            texpy.ConsoleLogger())
+    # Create logger
+    if opt.queit:
+        logger = texpy.NullLogger()
+    else:
+        logger = texpy.ConsoleLogger()
 
+    # Create the parser
+    parser = texpy.Parser(filename, fileobj, interactive, logger)
+
+    # Load latexstubs
     if opt.use_stubs:
         # Mimic the most important parts of LaTeX style
         import latexstubs
         latexstubs.initLaTeXstyle(parser)
+        whitelist = latexstubs.whitelistEnvironments
+    else:
+        whitelist = None
 
     doc = parser.parse()
     
     # Print tree
     if opt.print_tree:
         print doc.treeRepr()
+
+    # Print text
+    if opt.print_text:
+        print get_text(doc, whitelist)
 
 if __name__ == '__main__':
     main()
