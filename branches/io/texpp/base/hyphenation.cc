8d2e8ac927c455877bb3da94f16126a23e5977d8
ks.vladimir <ks.vladimir@7866b4b4-0b4f-11de-a021-8b77bb4f2096>
2009-05-10 22:17:15 +0000

    Disable saving of hyphenation.

--- branches/io/texpp/base/hyphenation.cc
+++ branches/io/texpp/base/hyphenation.cc
@@ -32,15 +32,16 @@ bool Hyphenation::invokeOperation(Parser& parser,
         Node::ptr internal = parser.parseGeneralText(true);
         Token::list_ptr tokens = internal->child("balanced_text")
                                     ->value(Token::list_ptr());
-        node->setValue(tokens ? *tokens : Token::list());
+        //node->setValue(tokens ? *tokens : Token::list());
 
         node->appendChild("rvalue", internal);
 
+        /*
         if(tokens) {
             Token::list h = parser.symbol(name, Token::list());
             std::copy(tokens->begin(), tokens->end(), std::back_inserter(h));
             parser.setSymbol(name, h, true); // global
-        }
+        }*/
         return true;
     }
     return false;
